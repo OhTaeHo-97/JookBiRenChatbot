68ca5a4756bc8d9c28fa0a071eb2dfc2b15790ba
otaeho <dhxogh123123@naver.com>
2024-02-06 23:11:01 +0900

    first commit

---
+++ JookBiRen/src/main/java/com/chatbot/JookBiRenApplication.java
@@ -0,0 +1,13 @@
+package com.chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JookBiRenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JookBiRenApplication.class, args);
+	}
+
+}
