900dc9055b5b75bfb73e93273cb49131c3260ff2
otaeho <dhxogh123123@naver.com>
2024-02-06 23:36:27 +0900

    feat(AnswerController): 유저 입력의 정답 여부 확인에 대한 핸들러 메서드 작성

---
+++ JookBiRen/src/main/java/com/chatbot/answer/controller/AnswerController.java
@@ -0,0 +1,27 @@
+package com.chatbot.answer.controller;
+
+import com.chatbot.answer.dto.AnswerDto.AnswerPostDto;
+import com.chatbot.answer.dto.AnswerDto.ResponseDto;
+import com.chatbot.answer.entity.Answer;
+import com.chatbot.answer.service.AnswerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@Validated
+public class AnswerController {
+    private final AnswerService answerService;
+
+    @PostMapping("/quiz")
+    public ResponseEntity quiz1(@RequestBody AnswerPostDto quizInfo) {
+        System.out.println("a");
+        ResponseDto response = answerService.checkAnswer(Answer.answerPostDtoToAnswer(quizInfo));
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
