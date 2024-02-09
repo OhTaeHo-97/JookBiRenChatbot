89a64f7bd2b7385dfa39abe70d433fc1b6dbfa2c
otaeho <dhxogh123123@naver.com>
2024-02-10 01:03:27 +0900

    feat(LoginBlockService): 로그인 성공 여부에 따른 블록 아이디 반환하는 비즈니스 메서드 작성

--- JookBiRen/src/main/java/com/chatbot/block/service/LoginBlockService.java
+++ JookBiRen/src/main/java/com/chatbot/block/service/LoginBlockService.java
@@ -1,2 +1,49 @@
-package com.chatbot.block.service;public class LoginBlockService {
+package com.chatbot.block.service;
+
+import com.chatbot.block.entity.LoginBlock;
+import com.chatbot.block.repository.LoginBlockRepository;
+import jakarta.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class LoginBlockService {
+    private final LoginBlockRepository loginBlockRepository;
+
+    public List<String> findLoginBlockByLogin(boolean isSuccessfulLogin) {
+        List<LoginBlock> loginBlocks = getLoginBlocks(isSuccessfulLogin);
+        return extractBlockIdInLoginBlocks(loginBlocks);
+    }
+
+    private List<LoginBlock> getLoginBlocks(boolean isSuccessfulLogin) {
+        if (isSuccessfulLogin) {
+            return getSuccessfulLoginBlocks();
+        }
+        return getUnsuccessfulLoginBlocks();
+    }
+
+    private List<LoginBlock> getSuccessfulLoginBlocks() {
+        List<LoginBlock> loginBlocks = new ArrayList<>();
+        LoginBlock loginBlock = loginBlockRepository.findByIsSuccessfulLoginAndIsTutorial(true, false).orElse(null);
+        LoginBlock tutorialLoginBlock = loginBlockRepository.findByIsSuccessfulLoginAndIsTutorial(true, true)
+                .orElse(null);
+        loginBlocks.add(loginBlock);
+        loginBlocks.add(tutorialLoginBlock);
+        return loginBlocks;
+    }
+
+    private List<LoginBlock> getUnsuccessfulLoginBlocks() {
+        List<LoginBlock> loginBlocks = new ArrayList<>();
+        LoginBlock loginBlock = loginBlockRepository.findByIsSuccessfulLogin(false).orElse(null);
+        loginBlocks.add(loginBlock);
+        return loginBlocks;
+    }
+
+    private List<String> extractBlockIdInLoginBlocks(List<LoginBlock> loginBlocks) {
+        return loginBlocks.stream().map(loginBlock -> loginBlock.getBlockId()).toList();
+    }
 }
