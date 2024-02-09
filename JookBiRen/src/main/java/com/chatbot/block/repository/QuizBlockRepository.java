323fc3eca86eefc5b75ff116f2e9dee6de51c2c3
otaeho <dhxogh123123@naver.com>
2024-02-10 00:13:53 +0900

    rename(QuizBlock): 퀴즈 블록 ID와 관련된 엔티티 이름 변경

---
+++ JookBiRen/src/main/java/com/chatbot/block/repository/QuizBlockRepository.java
@@ -0,0 +1,11 @@
+package com.chatbot.block.repository;
+
+import com.chatbot.block.entity.QuizBlock;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlockRepository extends JpaRepository<QuizBlock, Long> {
+    Optional<QuizBlock> findByEpisodeAndQuizNumberAndIsCorrectAndIsAnswer(int episode, int quizNumber,
+                                                                          boolean isCorrect,
+                                                                          boolean isAnswer);
+}
--- JookBiRen/src/main/java/com/chatbot/block/service/QuizBlockService.java
+++ JookBiRen/src/main/java/com/chatbot/block/service/QuizBlockService.java
@@ -1,6 +1,6 @@
 package com.chatbot.block.service;
 
-import com.chatbot.block.entity.Block;
+import com.chatbot.block.entity.QuizBlock;
 import com.chatbot.block.repository.BlockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,14 +11,14 @@ public class BlockService {
     private final BlockRepository blockRepository;
 
     public String findBlockId(int episode, int quizNumber, boolean isCorrect, boolean isAnswer) {
-        Block block = validateBlock(episode, quizNumber, isCorrect, isAnswer);
+        QuizBlock block = validateBlock(episode, quizNumber, isCorrect, isAnswer);
         if (block == null) {
             return new String();
         }
         return block.getBlockId();
     }
 
-    private Block validateBlock(int episode, int quizNumber, boolean isCorrect, boolean isAnswer) {
+    private QuizBlock validateBlock(int episode, int quizNumber, boolean isCorrect, boolean isAnswer) {
         return blockRepository.findByEpisodeAndQuizNumberAndIsCorrectAndIsAnswer(episode, quizNumber, isCorrect,
                 isAnswer).orElse(null);
     }
