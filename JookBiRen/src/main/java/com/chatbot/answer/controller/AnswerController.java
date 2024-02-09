b6a4c62203796e87b935e35f6beb46ac78129835
otaeho <dhxogh123123@naver.com>
2024-02-10 01:20:48 +0900

    feat(AnswerController): 모든 퀴즈에 대한 정답 확인 및 다음 블럭 아이디를 제공하기 위한 핸들러 메서드 작성

--- JookBiRen/src/main/java/com/chatbot/answer/controller/AnswerController.java
+++ JookBiRen/src/main/java/com/chatbot/answer/controller/AnswerController.java
@@ -18,8 +18,86 @@ import org.springframework.web.bind.annotation.RestController;
 public class AnswerController {
     private final AnswerService answerService;
 
-    @PostMapping("/quiz")
-    public ResponseEntity quiz1(@RequestBody AnswerPostDto quizInfo) {
+    @PostMapping("/quizInput00")
+    public ResponseEntity quiz00(@RequestBody AnswerPostDto quizInfo) {
+        ResponseDto response = answerService.checkAnswer(Answer.answerPostDtoToAnswer(quizInfo));
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("/quizInput11")
+    public ResponseEntity quiz11(@RequestBody AnswerPostDto quizInfo) {
+        ResponseDto response = answerService.checkAnswer(Answer.answerPostDtoToAnswer(quizInfo));
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("/quizInput12")
+    public ResponseEntity quiz12(@RequestBody AnswerPostDto quizInfo) {
+        ResponseDto response = answerService.checkAnswer(Answer.answerPostDtoToAnswer(quizInfo));
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("/quizInput13")
+    public ResponseEntity quiz13(@RequestBody AnswerPostDto quizInfo) {
+        ResponseDto response = answerService.checkAnswer(Answer.answerPostDtoToAnswer(quizInfo));
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("/quizInput14")
+    public ResponseEntity quiz14(@RequestBody AnswerPostDto quizInfo) {
+        ResponseDto response = answerService.checkAnswer(Answer.answerPostDtoToAnswer(quizInfo));
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("/quizInput15")
+    public ResponseEntity quiz15(@RequestBody AnswerPostDto quizInfo) {
+        ResponseDto response = answerService.checkAnswer(Answer.answerPostDtoToAnswer(quizInfo));
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("/quizInput16")
+    public ResponseEntity quiz16(@RequestBody AnswerPostDto quizInfo) {
+        ResponseDto response = answerService.checkAnswer(Answer.answerPostDtoToAnswer(quizInfo));
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("/quizInput17")
+    public ResponseEntity quiz17(@RequestBody AnswerPostDto quizInfo) {
+        ResponseDto response = answerService.checkAnswer(Answer.answerPostDtoToAnswer(quizInfo));
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("/quizInput21")
+    public ResponseEntity quiz21(@RequestBody AnswerPostDto quizInfo) {
+        ResponseDto response = answerService.checkAnswer(Answer.answerPostDtoToAnswer(quizInfo));
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("/quizInput22")
+    public ResponseEntity quiz22(@RequestBody AnswerPostDto quizInfo) {
+        ResponseDto response = answerService.checkAnswer(Answer.answerPostDtoToAnswer(quizInfo));
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("/quizInput23")
+    public ResponseEntity quiz23(@RequestBody AnswerPostDto quizInfo) {
+        ResponseDto response = answerService.checkAnswer(Answer.answerPostDtoToAnswer(quizInfo));
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("/quizInput24")
+    public ResponseEntity quiz24(@RequestBody AnswerPostDto quizInfo) {
+        ResponseDto response = answerService.checkAnswer(Answer.answerPostDtoToAnswer(quizInfo));
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("/quizInput32")
+    public ResponseEntity quiz32(@RequestBody AnswerPostDto quizInfo) {
+        ResponseDto response = answerService.checkAnswer(Answer.answerPostDtoToAnswer(quizInfo));
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("/quizInput33")
+    public ResponseEntity quiz33(@RequestBody AnswerPostDto quizInfo) {
         ResponseDto response = answerService.checkAnswer(Answer.answerPostDtoToAnswer(quizInfo));
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
