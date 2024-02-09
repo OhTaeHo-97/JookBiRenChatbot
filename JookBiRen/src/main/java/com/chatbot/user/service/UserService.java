ae75e2c9bc2556a9d59c8b7e445d258ae1ee8922
otaeho <dhxogh123123@naver.com>
2024-02-10 01:10:44 +0900

    refactor(UserService): 접속 코드 및 접속 아이디에 따른 로그인 진행하는 비즈니스 로직 작성

--- JookBiRen/src/main/java/com/chatbot/user/service/UserService.java
+++ JookBiRen/src/main/java/com/chatbot/user/service/UserService.java
@@ -1,2 +1,68 @@
-package com.chatbot.user.service;public class UserService {
+package com.chatbot.user.service;
+
+import com.chatbot.block.service.LoginBlockService;
+import com.chatbot.user.dto.UserDto.ResponseDto;
+import com.chatbot.user.entity.User;
+import com.chatbot.user.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class UserService {
+    private final UserRepository userRepository;
+    private final LoginBlockService loginBlockService;
+
+    @Value("${login.code.prefix}")
+    private String codePrefix;
+
+    public ResponseDto login(User userInfo) {
+        String code = userInfo.getCode();
+        if (code.contains(codePrefix)) {
+            code = code.replaceAll(codePrefix, "");
+        } else {
+            User user = User.of(userInfo.getCode(), null);
+            List<String> blockIds = getBlockIds(false);
+            return User.userToResponseDto(user, 4, blockIds.get(0), null);
+        }
+
+        User user = validateCode(code);
+        if (user == null) {
+            List<String> blockIds = getBlockIds(false);
+            return User.userToResponseDto(User.of(userInfo.getCode(), null), 4, blockIds.get(0), null);
+        }
+        if (user.isBanned()) {
+            return User.userToResponseDto(user, 2, null, null);
+        }
+        if (user.getFirstId() == null) {
+            user.setFirstId(userInfo.getFirstId());
+            userRepository.save(user);
+            List<String> blockIds = getBlockIds(true);
+            return User.userToResponseDto(user, 1, blockIds.get(0), blockIds.get(1));
+        }
+        if (user.getFirstId().equals(userInfo.getFirstId())) {
+            List<String> blockIds = getBlockIds(true);
+            return User.userToResponseDto(user, 1, blockIds.get(0), blockIds.get(1));
+        }
+        if (!user.getFirstId().equals(userInfo.getFirstId())) {
+            user.setSecondId(userInfo.getFirstId());
+            user.setBanned(true);
+            userRepository.save(user);
+            return User.userToResponseDto(User.of(userInfo.getCode(), userInfo.getFirstId()), 3, null, null);
+        }
+        List<String> blockIds = getBlockIds(false);
+        return User.userToResponseDto(User.of(userInfo.getCode(), null), 4, blockIds.get(0), null);
+    }
+
+    private List<String> getBlockIds(boolean isSuccessfulLogin) {
+        return loginBlockService.findLoginBlockByLogin(isSuccessfulLogin);
+    }
+
+    private User validateCode(String code) {
+        return userRepository.findByCode(code).orElse(null);
+    }
 }
