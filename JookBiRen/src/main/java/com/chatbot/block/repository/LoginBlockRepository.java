f515ec82ff70a9fc837768cc477c6345a2aaf65a
otaeho <dhxogh123123@naver.com>
2024-02-10 01:01:50 +0900

    feat(LoginBlockRepository): 로그인 여부에 따른 필요한 블럭 아이디를 가져오는 repository 생성

--- JookBiRen/src/main/java/com/chatbot/block/repository/LoginBlockRepository.java
+++ JookBiRen/src/main/java/com/chatbot/block/repository/LoginBlockRepository.java
@@ -1,2 +1,11 @@
-package com.chatbot.block.repository;public interface LoginBlockRepository {
+package com.chatbot.block.repository;
+
+import com.chatbot.block.entity.LoginBlock;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LoginBlockRepository extends JpaRepository<LoginBlock, Long> {
+    Optional<LoginBlock> findByIsSuccessfulLogin(boolean isSuccessfulLogin);
+
+    Optional<LoginBlock> findByIsSuccessfulLoginAndIsTutorial(boolean isSuccessfulLogin, boolean isTutorial);
 }
