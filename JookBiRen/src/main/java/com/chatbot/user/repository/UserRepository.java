7a534a96663146fcf16bb14f6e30360770f3b396
otaeho <dhxogh123123@naver.com>
2024-02-10 00:00:09 +0900

    feat(UserRepository): User 엔티티에 대한 repository 생성
    접속 코드에 따른 User 객체를 찾는 기능 추가

--- JookBiRen/src/main/java/com/chatbot/user/repository/UserRepository.java
+++ JookBiRen/src/main/java/com/chatbot/user/repository/UserRepository.java
@@ -1,2 +1,9 @@
-package com.chatbot.user.repository;public interface UserRepository {
+package com.chatbot.user.repository;
+
+import com.chatbot.user.entity.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByCode(String code);
 }
