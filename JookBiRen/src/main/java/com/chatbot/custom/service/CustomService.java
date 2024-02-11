dc8763afce056c29b35a809c5613fb2cae07589a
otaeho <dhxogh123123@naver.com>
2024-02-11 23:51:57 +0900

    feat(CustomService): 커스텀 잠금 해제를 위한 비즈니스 메서드 작성

--- JookBiRen/src/main/java/com/chatbot/custom/service/CustomService.java
+++ JookBiRen/src/main/java/com/chatbot/custom/service/CustomService.java
@@ -1,2 +1,42 @@
-package com.chatbot.custom.service;public class CustomService {
+package com.chatbot.custom.service;
+
+import com.chatbot.custom.dto.CustomDto.Output;
+import com.chatbot.custom.dto.CustomDto.ResponseDto;
+import com.chatbot.custom.dto.CustomDto.Template;
+import com.chatbot.custom.dto.CustomDto.TextCard;
+import com.chatbot.user.entity.User;
+import com.chatbot.user.service.UserService;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomService {
+    private final UserService userService;
+
+    public ResponseDto openCustom(User userInfo, int customLoc) {
+        User user = userService.findUserById(userInfo.getFirstId());
+        if (user != null) {
+            int customCode = user.getCustom();
+            if ((customCode & (1 << (2 - customLoc))) == 0) {
+                customCode ^= (1 << (2 - customLoc));
+                user.setCustom(customCode);
+                userService.updateUser(user);
+            }
+        }
+
+        return makeResponseDto();
+    }
+
+    private ResponseDto makeResponseDto() {
+        TextCard textCard = new TextCard();
+        List<Output> outputs = new ArrayList<>();
+        Output output = new Output(textCard);
+        outputs.add(output);
+        Template template = new Template(Collections.unmodifiableList(outputs));
+        return new ResponseDto(template);
+    }
 }
