f17b6e765710233cfed81c7297cca7b348e826ea
otaeho <dhxogh123123@naver.com>
2024-02-15 23:37:26 +0900

    chore(CustomBlock): 커스텀 이미지 생성 후 다음 블럭 아이디들을 나타내는 엔티티 생성

--- JookBiRen/src/main/java/com/chatbot/block/entity/CustomBlock.java
+++ JookBiRen/src/main/java/com/chatbot/block/entity/CustomBlock.java
@@ -1,2 +1,19 @@
-package com.chatbot.block.entity;public class CustomBlock {
+package com.chatbot.block.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class CustomBlock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String blockId;
+    private boolean isInitial;
 }
