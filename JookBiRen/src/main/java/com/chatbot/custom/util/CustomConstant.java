d283a7edde0a0975ce9488a42e5f5f186c0d3c24
otaeho <dhxogh123123@naver.com>
2024-02-15 23:17:06 +0900

    chore(CustomConstant): 커스텀 이미지 카테고리 상수 작성

--- JookBiRen/src/main/java/com/chatbot/custom/util/CustomConstant.java
+++ JookBiRen/src/main/java/com/chatbot/custom/util/CustomConstant.java
@@ -1,2 +1,9 @@
-package com.chatbot.custom.util;public class CustomConstant {
+package com.chatbot.custom.util;
+
+public class CustomConstant {
+    public static final String EYES = "eyes";
+    public static final String SUNGLASSES = "sunglasses";
+    public static final String SNOUT = "snout";
+    public static final String NECKLACE = "necklace";
+    public static final String POSE = "pose";
 }
