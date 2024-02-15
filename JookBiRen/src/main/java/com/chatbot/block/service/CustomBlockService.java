94a730b50aecfcd95d3e8e7f91960ebcb75085d3
otaeho <dhxogh123123@naver.com>
2024-02-15 23:39:04 +0900

    feat(CustomBlockService): 커스텀 이미지 생성 후의 블럭 아이디를 반환하는 비즈니스 메서드 생성

--- JookBiRen/src/main/java/com/chatbot/block/service/CustomBlockService.java
+++ JookBiRen/src/main/java/com/chatbot/block/service/CustomBlockService.java
@@ -1,2 +1,22 @@
-package com.chatbot.block.service;public class CustomBlockService {
+package com.chatbot.block.service;
+
+import com.chatbot.block.entity.CustomBlock;
+import com.chatbot.block.repository.CustomBlockRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class CustomBlockService {
+    private final CustomBlockRepository customBlockRepository;
+
+    public String findCustomBlockId(boolean isInitial) {
+        CustomBlock customBlock = customBlockRepository.findByIsInitial(isInitial).orElse(null);
+        if (customBlock == null) {
+            return "";
+        }
+        return customBlock.getBlockId();
+    }
 }
