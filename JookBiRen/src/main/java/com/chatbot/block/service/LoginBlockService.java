ba906834f1dc08aaa07bcf651b3002c0896ee422
otaeho <dhxogh123123@naver.com>
2024-02-22 01:29:25 +0900

    feat(LoginBlockService): 로그인 여부 및 커스텀 여부에 따른 블럭 아이디 검색 기능 추가

--- JookBiRen/src/main/java/com/chatbot/block/service/LoginBlockService.java
+++ JookBiRen/src/main/java/com/chatbot/block/service/LoginBlockService.java
@@ -14,36 +14,58 @@ import org.springframework.stereotype.Service;
 public class LoginBlockService {
     private final LoginBlockRepository loginBlockRepository;
 
-    public List<String> findLoginBlockByLogin(boolean isSuccessfulLogin) {
-        List<LoginBlock> loginBlocks = getLoginBlocks(isSuccessfulLogin);
+    public List<String> findLoginBlockByLogin(boolean isSuccessfulLogin, boolean isStory) {
+        List<LoginBlock> loginBlocks = getLoginBlocks(isSuccessfulLogin, isStory);
         return extractBlockIdInLoginBlocks(loginBlocks);
     }
 
-    private List<LoginBlock> getLoginBlocks(boolean isSuccessfulLogin) {
-        if (isSuccessfulLogin) {
+    private List<LoginBlock> getLoginBlocks(boolean isSuccessfulLogin, boolean isStory) {
+        if (isSuccessfulLogin && isStory) {
             return getSuccessfulLoginBlocks();
+        }
+        if (isSuccessfulLogin && !isStory) {
+            return getSuccessfulCustomLoginBlocks();
+        }
+        if (!isSuccessfulLogin && !isStory) {
+            return getUnsuccessfulCustomLoginBlocks();
         }
         return getUnsuccessfulLoginBlocks();
     }
 
     private List<LoginBlock> getSuccessfulLoginBlocks() {
         List<LoginBlock> loginBlocks = new ArrayList<>();
-        LoginBlock loginBlock = loginBlockRepository.findByIsSuccessfulLoginAndIsTutorial(true, false).orElse(null);
-        LoginBlock tutorialLoginBlock = loginBlockRepository.findByIsSuccessfulLoginAndIsTutorial(true, true)
+        LoginBlock loginBlock = loginBlockRepository.findByIsSuccessfulLoginAndIsTutorialAndIsStory(true, false, true)
                 .orElse(null);
+        LoginBlock tutorialLoginBlock = loginBlockRepository.findByIsSuccessfulLoginAndIsTutorialAndIsStory(true, true,
+                true).orElse(null);
         loginBlocks.add(loginBlock);
         loginBlocks.add(tutorialLoginBlock);
         return loginBlocks;
     }
 
+    private List<LoginBlock> getSuccessfulCustomLoginBlocks() {
+        List<LoginBlock> loginBlocks = new ArrayList<>();
+        LoginBlock loginBlock = loginBlockRepository.findByIsSuccessfulLoginAndIsStory(true, false).orElse(null);
+        loginBlocks.add(loginBlock);
+        return loginBlocks;
+    }
+
     private List<LoginBlock> getUnsuccessfulLoginBlocks() {
         List<LoginBlock> loginBlocks = new ArrayList<>();
-        LoginBlock loginBlock = loginBlockRepository.findByIsSuccessfulLogin(false).orElse(null);
+        LoginBlock loginBlock = loginBlockRepository.findByIsSuccessfulLoginAndIsStory(false, true).orElse(null);
+        loginBlocks.add(loginBlock);
+        return loginBlocks;
+    }
+
+    private List<LoginBlock> getUnsuccessfulCustomLoginBlocks() {
+        List<LoginBlock> loginBlocks = new ArrayList<>();
+        LoginBlock loginBlock = loginBlockRepository.findByIsSuccessfulLoginAndIsStory(false, false).orElse(null);
         loginBlocks.add(loginBlock);
         return loginBlocks;
     }
 
     private List<String> extractBlockIdInLoginBlocks(List<LoginBlock> loginBlocks) {
+        System.out.println(loginBlocks);
         return loginBlocks.stream().map(loginBlock -> loginBlock.getBlockId()).toList();
     }
 }
