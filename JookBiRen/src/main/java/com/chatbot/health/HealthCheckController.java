834ac8d99ec4e7230e7f99c51fedc1bf47486b90
otaeho <dhxogh123123@naver.com>
2024-03-02 22:20:24 +0900

    fix(HealthCheckController): Controller 어노테이션 없음에 따른 핸들러 메서드로의 요청 전달 불가 수정

--- JookBiRen/src/main/java/com/chatbot/health/HealthCheckController.java
+++ JookBiRen/src/main/java/com/chatbot/health/HealthCheckController.java
@@ -1,7 +1,9 @@
 package com.chatbot.health;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HealthCheckController {
     @GetMapping("/healthcheck")
     public String healthCheck() {
